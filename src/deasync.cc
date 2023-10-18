33256c9a30f6add5a173e5c1373e9a2c2563e8a1
Nik Rykov <rykov.nikas@gmail.com>
2023-10-18 02:43:29 +0200

    another apporach with deasync

--- src/deasync.cc
+++ src/deasync.cc
@@ -1,33 +1,44 @@
-#define NAPI_VERSION 3
-
 #include <uv.h>
 #include <node_api.h>
+#include <pthread.h>
 
-static uv_async_t async_handle;
+// Global mutex and condition variable
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+bool async_work_done = false;
 
-void async_cb(uv_async_t* handle) {
-    uv_loop_t* loop = handle->loop;
-    while (uv_run(loop, UV_RUN_NOWAIT) != 0); // Run the loop until no more tasks are pending
+void async_work_callback(uv_work_t* req) {
+    // ... Do your async work here ...
+
+    // Signal that async work is done
+    pthread_mutex_lock(&mutex);
+    async_work_done = true;
+    pthread_cond_signal(&cond);
+    pthread_mutex_unlock(&mutex);
 }
 
 napi_value Run(napi_env env, napi_callback_info info) {
     uv_loop_s* loop;
     napi_get_uv_event_loop(env, &loop);
 
-    // Initialize the async handle if not already initialized
-    if (!uv_is_active((uv_handle_t*)&async_handle)) {
-        uv_async_init(loop, &async_handle, async_cb);
-    }
+    uv_work_t work_req;
+    uv_queue_work(loop, &work_req, async_work_callback, NULL);
 
-    uv_async_send(&async_handle); // Trigger the async callback which runs the loop
+    pthread_mutex_lock(&mutex);
+    while (!async_work_done) {
+        pthread_cond_wait(&cond, &mutex);
+    }
+    async_work_done = false; // reset the flag
+    pthread_mutex_unlock(&mutex);
+
     return nullptr;
 }
 
 napi_value Init(napi_env env, napi_value exports) {
     napi_value fn_run;
     napi_create_function(env, "run", NAPI_AUTO_LENGTH, Run, NULL, &fn_run);
-    napi_set_named_property(env, exports, "run", fn_run);
 
+    napi_set_named_property(env, exports, "run", fn_run);
     return exports;
 }
 
