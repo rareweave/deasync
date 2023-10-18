8bdd010c37397250c239d88b528973608e88a48a
Nik Rykov <rykov.nikas@gmail.com>
2023-10-18 02:07:11 +0200

    v1.0.7

--- src/deasync.cc
+++ src/deasync.cc
@@ -3,26 +3,31 @@
 #include <uv.h>
 #include <node_api.h>
 
-static int run_depth = 0;
+static uv_async_t async_handle;
+
+void async_cb(uv_async_t* handle) {
+    uv_loop_t* loop = handle->loop;
+    while (uv_run(loop, UV_RUN_NOWAIT) != 0); // Run the loop until no more tasks are pending
+}
 
 napi_value Run(napi_env env, napi_callback_info info) {
     uv_loop_s* loop;
     napi_get_uv_event_loop(env, &loop);
 
-    run_depth++;
-    while (run_depth > 0) {
-        uv_run(loop, UV_RUN_NOWAIT);
-        run_depth--;
+    // Initialize the async handle if not already initialized
+    if (!uv_is_active((uv_handle_t*)&async_handle)) {
+        uv_async_init(loop, &async_handle, async_cb);
     }
-    
+
+    uv_async_send(&async_handle); // Trigger the async callback which runs the loop
     return nullptr;
 }
 
 napi_value Init(napi_env env, napi_value exports) {
     napi_value fn_run;
     napi_create_function(env, "run", NAPI_AUTO_LENGTH, Run, NULL, &fn_run);
-
     napi_set_named_property(env, exports, "run", fn_run);
+
     return exports;
 }
 
