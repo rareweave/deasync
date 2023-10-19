e0e19099763c7649808121502e90f7fd575c8c6f
Nik Rykov <rykov.nikas@gmail.com>
2023-10-19 20:01:34 +0200

    trying to fix it...

--- src/deasync.cc
+++ src/deasync.cc
@@ -2,42 +2,22 @@
 
 #include <uv.h>
 #include <node_api.h>
-
-uv_async_t async_handle;
-volatile bool is_inside_uv_run = false;
-
-void async_cb(uv_async_t* handle) {
-    // This will be called when our async event is dispatched.
-    // By calling uv_stop, we'll exit out of the uv_run loop.
-    uv_stop(handle->loop);
-}
+#include <chrono>
+#include <thread>
 
 napi_value Run(napi_env env, napi_callback_info info) {
     uv_loop_s* loop;
     napi_get_uv_event_loop(env, &loop);
 
-    if (is_inside_uv_run) {
-        // If already inside uv_run, trigger our async event
-        // to make sure the loop doesn't block indefinitely.
-        uv_async_send(&async_handle);
-        return nullptr;
+    // Non-blocking check, then sleep for a tiny duration before checking again.
+    while (uv_run(loop, UV_RUN_NOWAIT) != 0) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
-
-    is_inside_uv_run = true;
-    uv_async_send(&async_handle); // This will make sure there's always something in the event queue.
-    uv_run(loop, UV_RUN_ONCE);    // This will block until the above async send completes.
-    is_inside_uv_run = false;
 
     return nullptr;
 }
 
 napi_value Init(napi_env env, napi_value exports) {
-    uv_loop_s* loop;
-    napi_get_uv_event_loop(env, &loop);
-
-    // Initialize our async handle.
-    uv_async_init(loop, &async_handle, async_cb);
-
     napi_value fn_run;
     napi_create_function(env, "run", NAPI_AUTO_LENGTH, Run, NULL, &fn_run);
 
